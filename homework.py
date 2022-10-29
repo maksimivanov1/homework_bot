686e12629c6820c7f21ca03553fc7a26a247b5d1
Максим <maksim@MacBook-Air-Exon.local>
2022-10-29 20:22:10 +0300

    На последнее ревью

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import logging
 import os
 import time
 from urllib import response
-
+from http import HTTPStatus
 import requests
 import telegram
 from dotenv import load_dotenv
@@ -52,26 +52,26 @@ def get_api_answer(current_timestamp):
         'url': ENDPOINT,
         'headers': HEADERS,
         'params': params}
+        
     try:
         homework_statuses = requests.get(**req_dict)
     except Exception as error:
         raise SystemError(f'Ошибка получения request, {error}')
     else:
-        if homework_statuses.status_code == 200:
-            logger.info('успешное получение Эндпоинта')
-            homework = homework_statuses.json()
-            if 'error' in homework:
-                raise SystemError(f'Ошибка json, {homework["error"]}')
-            elif 'code' in homework:
-                raise SystemError(f'Ошибка json, {homework["code"]}')
-            else:
-                return homework
-        else:
+        if homework_statuses.status_code != HTTPStatus.OK:
             raise SystemError(
                 f'Ошибка!Статус: {homework_statuses.status_code},'
                 f'параметры запроса: {homework_statuses.headers},'
                 f' {homework_statuses.url}, {timestamp}'
                 f'контент ответа: {response.json()}.')
+        logger.info('успешное получение Эндпоинта')
+        homework = homework_statuses.json()
+        if 'error' in homework:
+            raise SystemError(f'Ошибка json, {homework["error"]}')
+        elif 'code' in homework:
+            raise SystemError(f'Ошибка json, {homework["code"]}')
+        else:
+            return homework
 
 
 def check_response(response):
@@ -80,9 +80,15 @@ def check_response(response):
     Если ключ [homeworks] не найден,
     то всё перехватится в main().
     """
-    if type(response) != dict:
+    if not isinstance(response, dict):
         raise TypeError('Ответ от API не словарь')
-    if type(response['homeworks']) == list:
+    if 'homeworks' not in response:
+        raise KeyError('В ответе от API отсутсвует ключ homeworks.')
+    if 'current_date' not in response:
+        raise KeyError('В ответе от API отсутсвует ключ current_date.')
+    if not isinstance(response['homeworks'], list):
+        raise TypeError('Тип homeworks не список.')
+    else:
         return response['homeworks']
 
 
@@ -90,6 +96,7 @@ def parse_status(homework):
     """Парсим информацию о домашке."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
+
     if homework_name is not None and homework_status is not None:
         if homework_status in HOMEWORK_STATUSES:
             verdict = HOMEWORK_STATUSES.get(homework_status)
@@ -98,7 +105,10 @@ def parse_status(homework):
         else:
             raise SystemError('Hеизвестный статус')
     else:
-        raise KeyError('Hет нужных ключей в словаре')
+        if homework_name is None:
+            raise KeyError('Ключ homework_name отсутсвует')
+        if homework_status is None:
+            raise KeyError('Ключ homework_status отсутсвует.')
 
 
 def check_tokens():
@@ -110,19 +120,20 @@ def main():
     """Основная логика работы."""
     if not check_tokens():
         raise SystemExit('Ошибка токенов.')
+
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()) - RETRY_TIME
+    current_timestamp = int(time.time())
     before_message = ''
+
     while True:
         try:
             if type(current_timestamp) is not int:
                 raise SystemError('Передана не дата.')
             response = get_api_answer(current_timestamp)
-            response = check_response(response)
-            if response is True:
+            homeworks_list = check_response(response)
+            if homeworks_list:
                 homework_status = parse_status(response[0])
-                if homework_status is not None:
-                    send_message(bot, homework_status)
+                send_message(bot, homework_status)
             else:
                 logger.debug('Новые статусы отсутсвуют.')
         except Exception as error:
